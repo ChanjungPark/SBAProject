37b36eed22d39022ce0ac13ebba7d52f921fe7cb
ChanjungPark <chan09062000@naver.com>
2020-09-25 16:10:26 +0900

    commit

--- member/student.py
+++ member/student.py
@@ -15,39 +15,42 @@ class StudentDao:
     
     def create(self):
         # cursor(커서) : 데이터베이스 작업을 수행하고 있는 메모리 공간
+        cursor = self.cursor
         try:
             # excute : sql 구문을 실행해주는 함수
-            self.cursor.execute("drop table students")
+            cursor.execute("drop table students")
         except sqlite3.OperationalError as err:
             print("테이블이 존재하지 않습니다.")
 
-        self.cursor.execute(
+        cursor.execute(
             '''create table students
             (id text primary key, pwd text, name text, birth text)'''
         )
-        self.cursor.commit()
+        self.conn.commit()
 
-    def insert_one(self, payload):
-        # payload = ('lee', '이승기', '1989/11/11')
-        sql = "insert into students(id, pwd, name, birth) values ('{student.id}', '{student.pwd}', '{student.name}', '{student.birth}')"
-        self.cursor.execute(sql)
-        self.cursor.commit()
+    def insert_one(self, student):
+        cursor = self.cursor
+        sql = """
+            insert into student(id, pwd, name, birth) values (?, ?, ?, ?)
+        """
+        cursor.execute(sql, (student.id, student.pwd, student.name, student.birth))
+        self.conn.commit()
 
     def insert_many(self):
-        data = [('jo', '조용필', '1985/12/31'), ('ko', '고아라', '1970/07/17'), ('sim', '심형래', '1950/06/06')]
-        # ?: place holder : 치환되어야 할 어떤 대상
-        # 데이터 유형에 상관없이 외따옴표 적지 X
-        query = """
-            INSERT INTO member(userid, password, phone) VALUES ('?, ?, ?')
-        """
-        self.cursor.executemany(query, data)
-        self.cursor.commit()
-
-    def fetch_one(self):
         cursor = self.cursor
-        findID = 'ko'
-        sql = "select * from students where id = '%s'" % (findID)
-        cursor.execute(sql)
+        data = [('jo','1', '조용필', '1985/12/31'), ('ko','1', '고아라', '1970/07/17'), ('sim','1', '심형래', '1950/06/06')]
+        # ?: place holder : 치환되어야 할 어떤 대상
+        # 데이터 유형에 상관없이 외따옴표 적지 마라
+        sql = """
+            insert into student(id, pwd, name, birth) values (?, ?, ?, ?)
+        """
+        cursor.executemany(sql, data)
+        self.conn.commit()
+
+    def fetch_by_id(self, id):
+        cursor = self.cursor
+        sql = "select * from students where id = '%s'"
+        cursor.execute(sql, (id))
         result = cursor.fetchone() # fetch 해줘야 함
         print(type(result)) # 튜플 형태로 리턴
         if result != None:
@@ -60,43 +63,53 @@ class StudentDao:
 
     def fetch_list(self):
         cursor = self.cursor
-        sql = 'select * from students order by name desc'
+        sql = """select * from students order by name desc"""
         for id, name, birth in cursor.execute(sql): # 다중 데이터는 for문으로 바로 출력가능
             print(id + '#' + name + '#' + birth)
         print('-'*20)
-
-    def fetch_by_name(self, name):
-        cursor = self.cursor
-        sql = f"select * from students where name like '%{name}%"
         cursor.execute(sql)
         return cursor.fetchall()
 
+    def fetch_by_name(self, name):
+        cursor = self.cursor
+        sql = """
+            select * from students where name like '%?%'
+        """
+        cursor.execute(sql, (name))
+        return cursor.fetchall() 
+
     def fetch_count(self):
         cursor = self.cursor
-        sql = f'select * from students'
-        return cursor.execute(sql)
+        sql = """select count(*) from students"""
+        cursor.execute(sql)
+        return cursor.fetchone()
 
     def fetch_all(self):
         cursor = self.cursor
         cursor.execute('select * from students')
         return cursor.fetchall()
-        
-    def login(self):
-        cursor = self.cursor
 
+    def login(self, id, pwd):
+        cursor = self.cursor
+        sql = """
+        select * from  students where id like ? and pwd like ?
+        """
+        cursor.execute(sql, (id, pwd))
+        return cursor.fetchone()
+        
     def update(self, id, name):
         # 'id'가 'lee'인 친구의 이름을 '이문세'로 바꾸세요.
         cursor = self.cursor
-        sql = f"update students set name = '{name}' where id = '{id}'"
-        cursor.execute(sql)
+        sql = """update students set name = ? where id = ?"""
+        cursor.execute(sql, (id, name))
         print(cursor.rowcount) # 성공 여부
-        cursor.commit()
+        self.conn.commit()
 
     def delete(self, id):
         # 'id' 데이터를 삭제하세요.
         cursor = self.cursor
-        sql = f"delete from students where id = '%{id}'"
-        cursor.execute(sql)
+        sql = """delete from students where id = ?"""
+        cursor.execute(sql, (id))
         print(cursor.rowcount)
         self.conn.commit()
 
@@ -104,4 +117,7 @@ class StudentDao:
         # conn.close()  # web 상에서는 close 하지 않음
 
 class StudentService:
-    pass
+    dao = StudentDao()
+
+    def add_students(self):
+        dao.insert_many()
